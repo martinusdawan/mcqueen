17b1ede807f14d34c1daa4ba8f3a0490eac8d255
martinuzdwn <martinuz.dawan9@gmail.com>
2019-09-22 09:15:05 +0700

    latihan kemarin

---
+++ code/mingguke_1/latihan1.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+
+nama = "Hello World"
+nomor = 1222222222222
+print(nomor)
+print(type(nama))
+print(type(nomor))
+
+# print(nomor)
+#print(nomor + len(nama))
+
+abc = ("aku", 12, 15.9, 100, koleksi)
+
+koleksi = ["Aku", "Ada", 12, 'W', 4.16, abc]
+#myset = set("b", "e")
+
+mykamus = {
+   "nama": "bambang",
+   "alamat": ["Jl, Jombor", "Jl. Monjali"],
+   "NomorTelp": "08",
+   "umur": 30,
+}
+
+
+print(koleksi)
+print(abc)
+print(myset)
+print(mykamus['umur'])
+
+
+if len(nama) < 50:
+    print("OKE")
+else:
+    print("salah")
+    
+
+if "nama" == "nama" and "nama" == "name":
+    print(1)
+else:
+    print(0)
+
+
+
+print("\n\n")
+
+if "nama" not in mykamus:
+    print(mykamus)
+else:
+    print(1)
+
+    
+print("\n\n")
+#num = input("Masukan kode: ")
+#print(num)
+
+
+#for (i=0; i<10; i++) {
+#}
+
+for i in range(10, 0, -1):
+    print(i)
+print("\n")
+for i in range(1, 10):
+    print(i)
+
+    
+    
+    
+    
+    
+    
+    
